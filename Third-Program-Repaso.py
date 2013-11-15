bfb112172eeeb96ee889769e6c77c0dea1f2f9e2
Luis94rueda <Luis_rueda2010@hotmail.com>
2013-11-15 14:58:16 -0500

    first commit

---
+++ Third-Program-Repaso.py
@@ -0,0 +1,109 @@
+#Numeros
+print "\t\t Numeros"
+print "Exponente 2**3=8"
+
+#Division
+print "\t\t Division"
+print "D=3.0/2, mostrar como decimales"
+D=3.0/2
+print D
+print "\n"
+
+#Division entera
+print "\t\t Division Entera"
+print "D=3//2"
+DE = 3//2
+print DE
+print "\n"
+
+#Modulo
+print "\t\t Modulo"
+print "Mod=3%2"
+DM=3.0%2
+print DM
+r"\n"
+DM1=3%2
+print DM1
+print "\n"
+
+#Operador and
+print "\t\t Operador and &"
+print "r=3&2"
+r=3&2
+print r
+print "\n"
+
+#operador or
+print "\t\t Operador or |"
+print "r=3|2"
+r=3|2
+print r
+print "\n"
+
+#operador xor
+print "\t\t Operador xor ^"
+print "r=3^2"
+r=3^2
+print r
+print "\n"
+
+#Operador not
+print "\t\t Operador not ~"
+print "r=~3"
+r=~3
+print r
+print "\n"
+
+#Caracteres
+print "\t\t Caracteres"
+a="Uno"
+print a
+b="Dos"
+print b
+
+print "c=a+b"
+c=a+b
+print c
+print "\n"
+
+print "c=a*3"
+c=a*3
+print c
+print "\n"
+
+#Booleanos
+print "\t\t\t Variable tipo booleano"
+print "true or false"
+print "and; or; not."
+
+print "\t\t\t OPERADORES \n"
+print "Igual (==); Diferente(!=); Menor o mayor que (<,>); Menor/mayor o igual que (<=,>=)"
+
+#Colecciones
+print "\t\t\t LISTAS (Arrays o Vectores)"
+l=[22, [True, False], "una lista", [1,2]]
+mi_var = l[1][1]
+print mi_var
+mi_var = l[-2]
+print mi_var
+
+#slicing
+mi_var = l[0:2]
+print mi_var
+
+#Tuplas
+
+
+
+
+
+
+
+print """
+\t\t\t      Luis Enrique Rueda Duarte
+\t\t\t      Estudiate Ing. Electronica
+\t\t\t  Universidad Industrial de Santander
+\t\t\t     copyright all rights reserved """
+
+raw_input()
+
